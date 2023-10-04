e82be8c08b9ae5b23a0dbd720b505bfe0fda4eee
kaiba <kaibadash@gmail.com>
2023-10-04 23:20:07 +0900

    Deploy to GH pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/KeyLinker">
       <Routes>
         <Route path={`/`} element={<Home />} />
         <Route path={`/:target`} element={<Home />} />
